c0fd722ca2e80811e1cd4a53083eee22d1cf3233
dbu <david.buchmann@liip.ch>
2011-05-03 13:59:10 +0200

    fixture loading needs explicit path

--- Test/CmfTestCase.php
+++ Test/CmfTestCase.php
@@ -13,17 +13,17 @@ use Liip\FunctionalTestBundle\Test\WebTestCase as BaseWebTestCase;
  * Connection information is read from jackalope.options... parameters
  *
  * Fixtures are loaded from PHPCR XML dumps.
- * To acquire some fixtures, create the necessary content in your repository, 
+ * To acquire some fixtures, create the necessary content in your repository,
  * then locate jack.jar inside phpcr-odm to dump the repository. You should
- * specify the base xpath to the name of the root node of your content. 
- * Without that, you will dump all kinds of basic definitions and get a file 
+ * specify the base xpath to the name of the root node of your content.
+ * Without that, you will dump all kinds of basic definitions and get a file
  * of 40+ M.
  *
  * java -jar /path/to/jack.jar export dump.xml repository-base-xpath=/cms
  *
  * Jack dumps the file on one single line. You can format that for better
  * reading, i.e. xmllint --format dump.xml > dump-formatted.xml
- * 
+ *
  * The document view is more readable than the system view. However, types
  * are not stored and you can get problems, i.e. with node references.
  *
@@ -37,25 +37,26 @@ class CmfTestCase extends BaseWebTestCase
 
     /**
      * setup this test
+     * for fixtures path, you typically use something like __DIR__/../Fixtures/
      *
-     * @param string $fixturesPath the path to the directory with the fixture xmls for testing. defaults to __DIR__/../Fixtures/
+     * @param string $fixturesPath the path to the directory with the fixture xmls for testing.
      */
-    public function __construct($fixturesPath = null)
+    public function __construct($fixturesPath)
     {
         parent::__construct();
 
-        if (is_null($fixturesPath)) {
-            $fixturesPath = __DIR__.'/../Fixtures/';
-        }
         $this->importexport = new \jackrabbit_importexport($fixturesPath);
     }
 
     /**
-     * load a fixture (system view or document view) into the repository, overwriting its current content
+     * load a fixture xml file (system view or document view) into the
+     * repository, overwriting its current content.
      *
-     * @param string $filename the path to the xml that should be loaded.
+     * the file is build from the fixturesPath, this name and the extension .xml
+     *
+     * @param string $name the name of the fixture to load
      */
-    public function loadFixture($filename)
+    public function loadFixture($name)
     {
         //TODO: improve importexport to have an other way to pass options
         $container = $this->getContainer();
