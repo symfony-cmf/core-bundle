6afb6e16cb590b7e24d8e713ef8968ac0ed35275
WouterJ <waldio.webdesign@gmail.com>
2013-04-22 16:21:56 +0200

    Added xml schema

--- DependencyInjection/SymfonyCmfCoreExtension.php
+++ DependencyInjection/SymfonyCmfCoreExtension.php
@@ -27,6 +27,16 @@ class SymfonyCmfCoreExtension extends Extension
         }
     }
 
+    /**
+     * Returns the base path for the XSD files.
+     *
+     * @return string The XSD base path
+     */
+    public function getXsdValidationBasePath()
+    {
+        return __DIR__.'/../Resources/config/schema';
+    }
+
     public function getNamespace()
     {
         return 'http://cmf.symfony.com/schema/dic/core';
