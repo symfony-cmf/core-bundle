87bd672020d074b063f1f33893f3d37e2ceaaca1
David Buchmann <david.buchmann@liip.ch>
2013-05-15 11:43:15 +0200

    add cmf_request_aware tag

---
+++ DependencyInjection/Compiler/RequestAwarePass.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\CoreBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+/**
+ * A compiler pass to find all cmf_request_aware services and add them to the
+ * RequestAwareListener
+ *
+ * @author David Buchmann <mail@davidbu.ch>
+ */
+class RequestAwarePass implements CompilerPassInterface
+{
+    /**
+     * Adds services tagged with cmf_request_aware to the RequestAwareListener
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('symfony_cmf_core.listener.request_aware')) {
+            return;
+        }
+
+        $listener = $container->getDefinition('symfony_cmf_core.listener.request_aware');
+        $services = $container->findTaggedServiceIds('cmf_request_aware');
+        foreach ($services as $id => $attributes) {
+            $listener->addMethodCall('addService', array(new Reference($id)));
+        }
+    }
+}
