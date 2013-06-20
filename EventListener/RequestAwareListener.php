a8b0ba10e1063e23e5917092b92dc2450a3ee8c8
David Buchmann <david.buchmann@liip.ch>
2013-06-20 12:54:38 +0200

    use symfony 2.3 synchronized services for cmf_request_aware and emulate them in symfony 2.2

--- EventListener/RequestAwareListener.php
+++ EventListener/RequestAwareListener.php
@@ -5,26 +5,41 @@ namespace Symfony\Cmf\Bundle\CoreBundle\EventListener;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Event\PostResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-use Symfony\Cmf\Bundle\MenuBundle\Voter\VoterInterface;
-
 /**
- * Set the master request on services tagged with cmf_request_aware
+ * Set and update the request on services tagged with cmf_request_aware
+ *
+ * This listener is emulating the Symfony 2.3 synchronized service behaviour
+ * in Symfony 2.2.
  *
  * The objects must have a method setRequest that accepts a Request as
- * parameter. If they don't, the problem is silently ignored.
+ * parameter. Note that the request may also be null, after the master
+ * request is terminated
  *
  * @author David Buchmann <mail@davidbu.ch>
  */
 class RequestAwareListener implements EventSubscriberInterface
 {
-    protected $services = array();
+    private $services = array();
+    private $requestStack = array();
 
     public function onKernelRequest(GetResponseEvent $event)
     {
+        $request = $event->getRequest();
+        array_push($this->requestStack, $request);
         foreach ($this->services as $service) {
-            $service->setRequest($event->getRequest());
+            $service->setRequest($request);
+        }
+    }
+
+    public function onKernelTerminate(PostResponseEvent $event)
+    {
+        array_pop($this->requestStack);
+        $request = empty($this->requestStack) ? null : end($this->requestStack);
+        foreach ($this->services as $service) {
+            $service->setRequest($request);
         }
     }
 
@@ -42,6 +57,7 @@ class RequestAwareListener implements EventSubscriberInterface
     {
         return array(
             KernelEvents::REQUEST => 'onKernelRequest',
+            KernelEvents::TERMINATE => 'onKernelTerminate',
         );
     }
 }
