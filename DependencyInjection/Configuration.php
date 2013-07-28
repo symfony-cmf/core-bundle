a2eff5f436f1cb8713c4f1efcdb04c54bd476bcd
Lukas Kahwe Smith <smith@pooteeweet.org>
2013-07-28 13:04:44 +0200

    copy paste error fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,7 +10,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cmf_content');
+        $rootNode = $treeBuilder->root('cmf_core');
 
         $rootNode
             ->children()
